925a8f6b3474503a6f2dba31cbcd7a5bfab2c0cb
지은입니다 <w2317@e-mirim.hs.kr>
2024-03-05 19:10:16 +0900

    예제5

---
+++ Student.java
@@ -0,0 +1,9 @@
+
+public class Student {
+	String name;
+	int score;
+	
+	public void printInfo() {
+		System.out.println("학생의 이름은 " + name + "이고, 학생의 점수는 " + score + "입니다.");
+	}
+}
---
+++ Test5.java
@@ -0,0 +1,12 @@
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		Student oh = new Student();
+		oh.name = "오지은";
+		oh.score = 100;
+		
+		oh.printInfo();
+	}
+
+}
