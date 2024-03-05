c35a293d8c580a166b7f2858a64b69ab5562165e
지은입니다 <w2317@e-mirim.hs.kr>
2024-03-05 19:10:51 +0900

    예제7

---
+++ Circle.java
@@ -0,0 +1,12 @@
+
+public class Circle {
+	private int radius;
+	
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+	public int getArea() {
+		return radius * radius;
+	}
+}
---
+++ Test7.java
@@ -0,0 +1,11 @@
+
+public class Test7 {
+
+	public static void main(String[] args) {
+		Circle circle = new Circle();
+		
+		circle.setRadius(5);
+		System.out.println(circle.getArea());
+	}
+
+}
