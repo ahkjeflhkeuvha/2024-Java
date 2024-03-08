11cd35f80a14940e2c07b803f13095ce689a20ce
지은입니다 <w2317@e-mirim.hs.kr>
2024-03-08 20:15:05 +0900

    예제4 수정

--- Test4.java
+++ Test4.java
@@ -5,17 +5,15 @@ public class Test4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String[] str = new String[10];
-		
-		int max = 0;
+
 		String maxStr = "";
 		
 		for(int i = 0; i<str.length; i++) {
 			System.out.print("문자열을 입력해주세요 > ");
-			String getStr = sc.next();
+			str[i] = sc.next();
 			
-			if(getStr.length() > max) {
-				max = getStr.length();
-				maxStr = getStr;
+			if(str[i].length() > maxStr.length()) {
+				maxStr = str[i];
 			}
 		}
 		
