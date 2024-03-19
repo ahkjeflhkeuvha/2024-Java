0a22eed87a45a69349b90592352b70f6a83e86b0
지은입니다 <w2317@e-mirim.hs.kr>
2024-03-19 19:22:53 +0900

    상속 예제 3

--- Point.java
+++ Point.java
@@ -1,5 +1,16 @@
 public class Point {
 	private int x, y;
+	
+	public Point() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 	public void set (int x, int y) {
 		this.x = x;
 		this.y = y;
